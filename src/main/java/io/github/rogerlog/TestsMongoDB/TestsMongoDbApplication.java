54f02dc61462b9dd8aecd1ebf79d12c86e78b187
Roger <logroger@gmail.com>
2021-04-19 17:13:22 -0300

    mongo base

---
+++ src/main/java/io/github/rogerlog/TestsMongoDB/TestsMongoDbApplication.java
@@ -0,0 +1,13 @@
+package io.github.rogerlog.TestsMongoDB;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestsMongoDbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestsMongoDbApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/github/rogerlog/TestsMongoDB/TestsMongoDbApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.rogerlog.TestsMongoDB;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestsMongoDbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
