659f6410964830006181a592badd1a7a81240d34
Roger <logroger@gmail.com>
2021-04-19 17:31:05 -0300

    User

---
+++ src/main/java/io/github/rogerlog/TestsMongoDB/domain/User.java
@@ -0,0 +1,41 @@
+package io.github.rogerlog.TestsMongoDB.domain;
+
+import java.io.Serializable;
+import java.util.Objects;
+
+public class User implements Serializable {
+    private String id;
+    private String name;
+    private String email;
+
+    public User(){
+
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        User user = (User) o;
+        return Objects.equals(id, user.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
+
+    public User(String id, String name, String email) {
+        this.id = id;
+        this.name = name;
+        this.email = email;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+}
