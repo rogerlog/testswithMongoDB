140ef6094b2e276fcf04bb65c955aad01264ef60
Roger <logroger@gmail.com>
2021-04-19 18:25:12 -0300

    Conectando com o MongoDB

--- src/main/java/io/github/rogerlog/TestsMongoDB/domain/User.java
+++ src/main/java/io/github/rogerlog/TestsMongoDB/domain/User.java
@@ -1,15 +1,29 @@
 package io.github.rogerlog.TestsMongoDB.domain;
 
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
 import java.io.Serializable;
 import java.util.Objects;
 
+@Document(collection="user")
 public class User implements Serializable {
+    private static final long serialVersionUID = 1L;
+
+    @Id
     private String id;
     private String name;
     private String email;
 
     public User(){
 
+    }
+
+    public User(String id, String name, String email) {
+        super();
+        this.id = id;
+        this.name = name;
+        this.email = email;
     }
 
     @Override
@@ -25,11 +39,23 @@ public class User implements Serializable {
         return Objects.hash(id);
     }
 
-    public User(String id, String name, String email) {
-        this.id = id;
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
         this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
         this.email = email;
     }
+
+
 
     public String getId() {
         return id;
--- src/main/java/io/github/rogerlog/TestsMongoDB/resources/UserResource.java
+++ src/main/java/io/github/rogerlog/TestsMongoDB/resources/UserResource.java
@@ -1,2 +1,28 @@
-package io.github.rogerlog.TestsMongoDB.resources;public class UserResource {
+package io.github.rogerlog.TestsMongoDB.resources;
+
+import io.github.rogerlog.TestsMongoDB.domain.User;
+
+import io.github.rogerlog.TestsMongoDB.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+@RestController
+@RequestMapping(value="/users")
+public class UserResource {
+
+    @Autowired
+    private UserService service;
+
+    @RequestMapping(method= RequestMethod.GET)
+    public ResponseEntity<List<User>> findAll() {
+        List<User> list = service.findAll();
+        return ResponseEntity.ok().body(list);
+    }
 }
