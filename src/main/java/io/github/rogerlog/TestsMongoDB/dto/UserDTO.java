ef51a5810921f0685b779b8179724418f88153fb
Roger <logroger@gmail.com>
2021-04-21 20:11:08 -0300

    DTO standard to return user

---
+++ src/main/java/io/github/rogerlog/TestsMongoDB/dto/UserDTO.java
@@ -0,0 +1,35 @@
+package io.github.rogerlog.TestsMongoDB.dto;
+
+import io.github.rogerlog.TestsMongoDB.domain.User;
+
+import java.io.Serializable;
+
+public class UserDTO implements Serializable {
+    private static final long serialVersionUID = 1L;
+
+    private String id;
+    private String name;
+    private String email;
+
+    public UserDTO() {
+
+    }
+
+    public UserDTO(User obj) {
+        id = obj.getId();
+        name = obj.getName();
+        email = obj.getEmail();
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+}
--- src/main/java/io/github/rogerlog/TestsMongoDB/resources/UserResource.java
+++ src/main/java/io/github/rogerlog/TestsMongoDB/resources/UserResource.java
@@ -2,6 +2,7 @@ package io.github.rogerlog.TestsMongoDB.resources;
 
 import io.github.rogerlog.TestsMongoDB.domain.User;
 
+import io.github.rogerlog.TestsMongoDB.dto.UserDTO;
 import io.github.rogerlog.TestsMongoDB.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -12,6 +13,7 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping(value="/users")
@@ -21,8 +23,9 @@ public class UserResource {
     private UserService service;
 
     @RequestMapping(method= RequestMethod.GET)
-    public ResponseEntity<List<User>> findAll() {
+    public ResponseEntity<List<UserDTO>> findAll() {
         List<User> list = service.findAll();
-        return ResponseEntity.ok().body(list);
+        List<UserDTO> listDto = list.stream().map(x -> new UserDTO(x)).collect(Collectors.toList());
+        return ResponseEntity.ok().body(listDto);
     }
 }
