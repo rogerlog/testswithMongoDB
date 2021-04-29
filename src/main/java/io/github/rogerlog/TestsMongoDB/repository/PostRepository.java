890690fd476f4e8aaccd28d6b349706f72d99c25
Roger <logroger@gmail.com>
2021-04-28 23:45:07 -0300

    simple search by query methods

--- src/main/java/io/github/rogerlog/TestsMongoDB/repository/PostRepository.java
+++ src/main/java/io/github/rogerlog/TestsMongoDB/repository/PostRepository.java
@@ -4,8 +4,10 @@ import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
 import io.github.rogerlog.TestsMongoDB.domain.Post;
+import java.util.List;
 
 @Repository
 public interface PostRepository extends MongoRepository<Post, String> {
 
+    List<Post> findByTitleContainingIgnoreCase(String text);
 }
