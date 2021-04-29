1c3d3c64b7256cae0076799e1a88d6f0c449e6cd
Roger <logroger@gmail.com>
2021-04-28 23:55:35 -0300

    fullsearch date

--- src/main/java/io/github/rogerlog/TestsMongoDB/repository/PostRepository.java
+++ src/main/java/io/github/rogerlog/TestsMongoDB/repository/PostRepository.java
@@ -1,5 +1,7 @@
 package io.github.rogerlog.TestsMongoDB.repository;
 
+import java.util.Date;
+
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
@@ -15,4 +17,7 @@ public interface PostRepository extends MongoRepository<Post, String> {
     List<Post> searchTitle(String text);
 
     List<Post> findByTitleContainingIgnoreCase(String text);
+
+    @Query("{ $and: [ { date: {$gte: ?1} }, { date: { $lte: ?2} } , { $or: [ { 'title': { $regex: ?0, $options: 'i' } }, { 'body': { $regex: ?0, $options: 'i' } }, { 'comments.text': { $regex: ?0, $options: 'i' } } ] } ] }")
+    List<Post> fullSearch(String text, Date minDate, Date maxDate);
 }
--- src/main/java/io/github/rogerlog/TestsMongoDB/services/PostService.java
+++ src/main/java/io/github/rogerlog/TestsMongoDB/services/PostService.java
@@ -1,6 +1,7 @@
 package io.github.rogerlog.TestsMongoDB.services;
 
 import java.util.List;
+import java.util.Date;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -25,5 +26,10 @@ public class PostService {
 
     public List<Post> findByTitle(String text) {
         return repo.searchTitle(text);
+    }
+
+    public List<Post> fullSearch(String text, Date minDate, Date maxDate) {
+        maxDate = new Date(maxDate.getTime() + 24 * 60 * 60 * 1000);
+        return repo.fullSearch(text, minDate, maxDate);
     }
 }
